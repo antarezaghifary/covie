4c89b885066321b7068399f9143a74f793ff726a
antareza <antareza.mustain@pcsindonesia.co.id>
2024-11-30 07:44:04 +0700

    : refactoring - navigate from splash to home

--- app/src/main/java/com/example/coremovie/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/coremovie/presentation/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.coremovie.presentation.main
 
-import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock
-import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -18,11 +14,6 @@ import androidx.navigation.fragment.NavHostFragment
 import com.example.coremovie.R
 import com.example.coremovie.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,26 +23,9 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         installSplashScreen()
-        val splashView = LayoutInflater.from(this).inflate(R.layout.splash_screen_layout, null)
-        val textView = splashView.findViewById<TextView>(R.id.splash_text)
-        textView.postDelayed({
-            textView.visibility = View.VISIBLE
-            ObjectAnimator.ofFloat(textView, "alpha", 0f, 1f).apply {
-                duration = 1000
-                start()
-            }
-        }, 1000)
-        setContentView(splashView)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
-            val fadeOut = ObjectAnimator.ofFloat(splashView, "alpha", 1f, 0f)
-            fadeOut.duration = 1000
-            fadeOut.start()
-        }
-
+        setContentView(binding.root)
         initUI()
     }
 
@@ -79,19 +53,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(3000)
-            setContentView(binding.root)
-            ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-            val navHostFragment =
-                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_bottem_navigation) as NavHostFragment?
-            val navController = navHostFragment?.navController
-            navController?.navigate(R.id.nav_graph)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_bottem_navigation) as NavHostFragment?
+        val navController = navHostFragment?.navController
+        navController?.navigate(R.id.nav_graph)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/coremovie/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/coremovie/presentation/splash/SplashFragment.kt
@@ -1,13 +1,20 @@
 package com.example.coremovie.presentation.splash
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.coremovie.R
 import com.example.coremovie.databinding.FragmentSplashBinding
+import com.example.coremovie.presentation.base.BaseFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashFragment : Fragment() {
+class SplashFragment : BaseFragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
@@ -22,5 +29,35 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            splashText.postDelayed({
+                splashText.visibility = View.VISIBLE
+                ObjectAnimator.ofFloat(splashText, "alpha", 0f, 1f).apply {
+                    duration = 1000
+                    start()
+                }
+            }, 1000)
+
+            lifecycleScope.launch(Dispatchers.Main) {
+                delay(3000)
+                if (findNavController().currentDestination?.id == R.id.splashFragment)
+                    findNavController().navigate(
+                        R.id.action_splashFragment_to_homeFragment
+                    )
+            }
+        }
+    }
+
+    override fun initOnClick() {
+
+    }
+
+    override fun collectState() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
